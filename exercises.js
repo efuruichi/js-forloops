ae44d714355a250b2376fa964b4ac72b70c09652
Erin Furuichi <erinf@Erins-MacBook-Pro.local>
2018-04-11 19:19:01 -1000

    forloops3

--- exercises.js
+++ exercises.js
@@ -9,7 +9,7 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 .
 "Now serving 20."
 */
-var text = "Now serving"
+var text = "Now serving";
 for (var i = 1; i <= 20; i++){
 
 console.log(text, i + ".");
@@ -50,12 +50,36 @@ This function will iterate through the person parameter and console.log the foll
 
 "President person was a great leader."
 */
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+
+for (var i = 0; i < presidents.length; i++){
+  console.log('The value at ' + i + ' is ' + presidents[i] + '.');
+}
+
+console.log(presidents.length);
+
+
+function leaders(person){
+  for (var i = 0; i < person.length; i++){
+console.log('President ' + person[i] + ' was a great leader.');
+  }
+  return person;
+
+  }
+console.log(leaders(presidents));
+
 
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
+//var stringOfNumbers = ''
+
+//for (var i = 10; i <= 20; i++){
+
+//}
+//console.log(stringOfNumbers(i));
 
 
 /* 5) Even Stevens
@@ -64,6 +88,16 @@ Declare a variable named `evenNumArr` and assign its value to an empty array.
 Write a for-loop that will push even numbers to the `evenNumArr` array. We want to push 50 even even numbers starting from 0.
 Console.log your results.
 */
+
+var evenNumArr = [];
+
+for (var i = 0; i <100; i++){
+  if(i%2 === 0){
+
+    evenNumArr.push(i);
+  }
+}
+console.log(evenNumArr);
 
 
 /* 6) Up the Odds
